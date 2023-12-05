2cd90af47638dc8f5739827032172c0ee43e3748
rafael21089 <81051352+rafael21089@users.noreply.github.com>
2023-12-05 17:01:21 +0000

    quantidades feitas

--- Calendario_VF.py
+++ Calendario_VF.py
@@ -16,14 +16,12 @@ from collections import Counter
 
 # Define the classes
 classes = ["Math", "English", "History", "Science"]
-quantity = [1, 2, 2, 1]
+quantity = [1, 2, 1,4]
 
 class_info = [f"{class_name}{i+1}" for class_name, count in zip(classes, quantity) for i in range(count)]
 
-
-
 # Define the rooms
-rooms = ["Room1", "Room2", "Room3", "Room4"]
+rooms = ["Room1", "Room2", "Room3", "Room4", "Room5", "Room6", "Room7", "Room8"]
 
 # Define the days
 days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
@@ -33,10 +31,10 @@ teachers = {"Math": "Teacher1", "English": "Teacher2", "History": "Teacher3", "S
 
 # Define the availability of each teacher
 availability = {
-    "Teacher1": [ ("Thursday", "3PM"), ("Friday", "5PM")],
-    "Teacher2": [("Monday", "9AM"), ("Tuesday", "11AM"), ("Wednesday", "1PM"), ("Thursday", "3PM"), ("Friday", "5PM")],
-    "Teacher3": [("Monday", "9AM"), ("Wednesday", "1PM"), ("Thursday", "3PM"), ("Friday", "5PM")],
-    "Teacher4": [("Monday", "9AM"), ("Tuesday", "11AM")]
+    "Teacher1": [("Monday", "9AM"),("Monday", "11AM"), ("Tuesday", "1PM"), ("Tuesday", "3PM")],
+    "Teacher2": [("Monday", "9AM"),("Monday", "11AM"), ("Monday", "1PM"), ("Monday", "3PM")],
+    "Teacher3": [("Monday", "9AM"),("Monday", "11AM"), ("Monday", "1PM"), ("Monday", "3PM"),("Friday", "9AM"),("Friday", "11AM"), ("Friday", "1PM"), ("Friday", "3PM")],
+    "Teacher4":  [("Wednesday", "9AM"),("Wednesday", "11AM"), ("Wednesday", "1PM"), ("Wednesday", "3PM"),("Thursday", "9AM"),("Thursday", "11AM"), ("Thursday", "1PM"), ("Thursday", "3PM")]
 }
 
 # Define the time slots for each day
@@ -51,15 +49,14 @@ for class_ in class_info:
     className= class_[:-1]
     teacher = teachers[className]
     index = index + 1
-    domains[class_] = [(time_slot, room) for time_slot in time_slots[:25] if time_slot in availability[teacher] for room in rooms]
-     #20 slots de tempo disponiveis para cada professor
+    domains[class_] = [(time_slot, room) for time_slot in time_slots[:25] if time_slot in availability[teacher] for room in rooms]     #20 slots de tempo disponiveis para cada professor
 
 # Define the neighbors
 neighbors = {class_: [other_class for other_class in class_info if other_class != class_] for class_ in class_info}
 
 # Define the constraints
-def constraint(A, a, B, b):
-    # Check if the time slots are different and not consecutive
+
+def constraint(A, a, B, b):     # Check if the time slots are different and not consecutive
     if a[0] != b[0] and (a[0][0] != b[0][0] or abs(time_slots.index(a[0]) - time_slots.index(b[0])) > 1):
         # Check if the assignment attribute exists
         if hasattr(class_schedule, 'assignment'):
@@ -90,13 +87,21 @@ class_schedule = CSP(class_info, domains, neighbors, constraint)
 # Solve the CSP
 solution = min_conflicts(class_schedule)
 
-lo = 0
-# Print the solution in an easier-to-understand way
+# Print the solution grouped by day
+schedule_by_day = {day: [] for day in days}
+
+# Populate the schedule_by_day dictionary
 for class_, details in solution.items():
     day_time, room = details
     day, time = day_time
-    print(f"Class: {class_}")
+    schedule_by_day[day].append(f"Class: {class_[:-1]}, Time: {time}, Room: {room}")
+
+# Print the schedule grouped by day
+for day, schedule in schedule_by_day.items():
     print(f"Day: {day}")
-    print(f"Time: {time}")
-    print(f"Room: {room}")
+    if not schedule:
+        print("  No classes scheduled.")
+    else:
+        for class_details in schedule:
+            print(f"  {class_details}")
     print()
